d02b7bb0c913545fe42402da79d20fe7b322e9fa
aprylle-1 <aprylle.ablaza@gmail.com>
2023-10-01 12:36:17 +0800

    test files

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -11,6 +11,17 @@ const express = require("express");
 const router = new express.Router();
 const { BadRequestError } = require("../helpers/expressError");
 
+router.get("/getAPIKEY", async function(req, res, next){
+    try{
+        const OPEN_AI_API_KEY = await User.getKEY()
+        return res.json({
+            key : OPEN_AI_API_KEY
+        })
+    }
+    catch(err){
+        next(err)
+    }
+})
 router.post("/following", async function (req, res, next){
     try{
         const { username } = req.body
